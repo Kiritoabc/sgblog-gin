5b62eaf75eef3d141e84b587c93ea6fbfad48013
Kirito <2493381254@qq.com>
2024-02-10 14:05:18 +0800

    update

--- app/admin/cmd/core/internal/constant.go
+++ app/admin/cmd/core/internal/constant.go
@@ -2,7 +2,7 @@ package internal
 
 const (
 	ConfigEnv         = "SG_BLOG_CONFIG"
-	ConfigDefaultFile = "admin.yaml"
+	ConfigDefaultFile = "app/admin/cmd/etc/admin.yaml"
 	ConfigTestFile    = "config.test.yaml"
 	ConfigDebugFil    = "config.debug.yaml"
 	ConfigReleaseFile = "config.release.yaml"
--- app/blog/cmd/core/internal/constant.go
+++ app/blog/cmd/core/internal/constant.go
@@ -2,7 +2,7 @@ package internal
 
 const (
 	ConfigEnv         = "SG_BLOG_CONFIG"
-	ConfigDefaultFile = "admin.yaml"
+	ConfigDefaultFile = "app/blog/cmd/etc/blog.yaml"
 	ConfigTestFile    = "config.test.yaml"
 	ConfigDebugFil    = "config.debug.yaml"
 	ConfigReleaseFile = "config.release.yaml"
